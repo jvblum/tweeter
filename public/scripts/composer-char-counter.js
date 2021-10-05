bf985f8d9d91476e96eeb830e1fc235016052294
jvblum <jvmell.skch@gmail.com>
2021-10-05 16:50:28 -0400

    modify composer-char-counter.js - remove devstuff

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -18,24 +18,10 @@ $(document).ready(() => {
 
     // assigns value of inner element as displayCount (character limit subtracted by character count)
     $('.char-counter').text(displayCount);
-    console.log(charCount);
-    console.log(displayCount);
 
   };
 
-
+  // event listener
   composerEvent.on('keyup', charCounter);
 
 });
-
-
-// const composerEvent = document.querySelector('#tweet-text');
-// let charCount = 0;
-
-// // callback for event listener; assigns the length of the value of #tweet-text
-// const charCounter = (e) => {
-//   charCount = composerEvent.value.length;
-//   console.log(charCount);
-// };
-
-// composerEvent.addEventListener('keyup', charCounter);
