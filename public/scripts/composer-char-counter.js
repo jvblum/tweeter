1ec34ef321b0ea1a7d232828d2336f292fcfff1e
jvblum <jvmell.skch@gmail.com>
2021-10-05 16:36:59 -0400

    modify character counter; jquery and display

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,32 @@
-const composerEvent = document.querySelector('#tweet-text');
-let charCount = 0;
+$(document).ready(() => {
+  const composerEvent = $('#tweet-text');
+  let charCount = 0;
 
-// callback for event listener; assigns the length of the value of #tweet-text
-const charCounter = (e) => {
-  charCount = composerEvent.value.length;
-  console.log(charCount);
-};
+  // callback for event listener; assigns the length of the value of #tweet-text
+  const charCounter = (e) => {
+    charCount = composerEvent.val().length;
+    charLimit = 140;
 
-composerEvent.addEventListener('keyup', charCounter);
+    displayCount = charLimit - charCount;
+
+    // assigns value of inner element as displayCount (character limit subtracted by character count)
+    $('.char-counter').text(displayCount);
+    console.log(charCount);
+    console.log(displayCount);
+  };
+
+  composerEvent.on('keyup', charCounter);
+
+});
+
+
+// const composerEvent = document.querySelector('#tweet-text');
+// let charCount = 0;
+
+// // callback for event listener; assigns the length of the value of #tweet-text
+// const charCounter = (e) => {
+//   charCount = composerEvent.value.length;
+//   console.log(charCount);
+// };
+
+// composerEvent.addEventListener('keyup', charCounter);
