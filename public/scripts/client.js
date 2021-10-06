c141c9f1a4d44e96eafb1a3ef32e887555cc71b2
jvblum <jvmell.skch@gmail.com>
2021-10-06 18:36:32 -0400

    implement basic error handling for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,38 +10,59 @@ $(document).ready(() => {
   // takes tweet object; returns html article of tweet
   const createTweetElement = tweet => {
     
-    // write elements 
-    const $avatars = $(`<img class="avatar" src="${tweet.user.avatars}">`);
-    const $name = $('<span class="name">').text(tweet.user.name);
-    const $handle = $('<span class="handle">').text(tweet.user.handle);
-    const $content = $('<p>').text(tweet.content.text);
-    const $timeago = $('<span>').text(timeago.format(tweet.created_at));
+    return $(`
+      <article>
+        <header>
+          <div class="user">
+            <img class="avatar" src="${tweet.user.avatars}">
+            <span class="name">${tweet.user.name}</span>
+          </div>
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+        <p>${tweet.content.text}</p><hr>
+        <footer>
+          <span>${timeago.format(tweet.created_at)}</span>
+            <span class="icons">
+              <i class="fas fa-flag" id="flag">
+              </i><i class="fas fa-retweet" id="retweet">
+              </i><i class="fas fa-heart" id="heart">
+              </i></span>
+        </footer>
+      </article>`
+    );
 
-    const $flag = $('<i class="fas fa-flag" id="flag">');
-    const $retweet = $('<i class="fas fa-retweet" id="retweet">');
-    const $heart = $('<i class="fas fa-heart" id="heart"></i>');
+    // // write elements
+    // const $avatars = $(`<img class="avatar" src="${tweet.user.avatars}">`);
+    // const $name = $('<span class="name">').text(tweet.user.name);
+    // const $handle = $('<span class="handle">').text(tweet.user.handle);
+    // const $content = $('<p>').text(tweet.content.text);
+    // const $timeago = $('<span>').text(timeago.format(tweet.created_at));
 
-    const $line = $('<hr>');
+    // const $flag = $('<i class="fas fa-flag" id="flag">');
+    // const $retweet = $('<i class="fas fa-retweet" id="retweet">');
+    // const $heart = $('<i class="fas fa-heart" id="heart"></i>');
 
-    // init containers
-    const $tweet = $('<article>');
+    // const $line = $('<hr>');
 
-    const $header = $('<header>');
-    const $user = $('<div class="user">');
+    // // init containers
+    // const $tweet = $('<article>');
 
-    const $footer = $('<footer>');
-    const $icons = $('<span class="icons">');
+    // const $header = $('<header>');
+    // const $user = $('<div class="user">');
 
-    // add elements to container
-    $user.append($avatars, $name);
-    $header.append($user, $handle);
+    // const $footer = $('<footer>');
+    // const $icons = $('<span class="icons">');
 
-    $icons.append($flag, $retweet, $heart);
-    $footer.append($timeago, $icons);
+    // // add elements to container
+    // $user.append($avatars, $name);
+    // $header.append($user, $handle);
 
-    $tweet.append($header, $content, $line, $footer);
+    // $icons.append($flag, $retweet, $heart);
+    // $footer.append($timeago, $icons);
+
+    // $tweet.append($header, $content, $line, $footer);
     
-    return $tweet;
+    // return $tweet;
   };
 
   const renderTweets = tweets => {
@@ -52,60 +73,58 @@ $(document).ready(() => {
     
     // render tweets
     for (const tweet of tweets) {
+      
       const $tweet = createTweetElement(tweet);
       $container.prepend($tweet);
+
     }
     
-  }
+  };
 
-    const $form = $(".new-tweet");
+  const $form = $('.new-tweet');
 
-    // new-tweet post request
-    $form.on("submit", function(event) {
+  // new-tweet post request
+  $form.on('submit', function(event) {
 
-      // stops refresh
-      event.preventDefault();
-      console.log('form was submitted');
-      
-      const serializedData = $(this).serialize();
-      console.log(this);
-      console.log(serializedData);
-      $.post("/tweets", serializedData, (response) => {
+    // stops refresh
+    event.preventDefault();
 
-        console.log(response);
-        renderTweets(data);
-        
-      });
+    // init submitted data
+    const input = $('#tweet-text').val();
+    const serializedData = $(this).serialize();
+
+    // bad requests
+    if (!input) {
+      return alert('write something!');
+    }
+    if (input.length > 140) {
+      return alert('tweet too long');
+    }
+    
+    // clears textarea
+    $('#tweet-text').val('');
+
+    // submits input to database; then renders page;
+    $.post('/tweets', serializedData, (response) => {
+
+      loadTweets();
 
     });
 
-    renderTweets(data);
-    
   });
 
-  // sample database
+    
+  // gets data from tweets then renders them
+  const loadTweets = () => {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      }
-    ]
+    $.get('/tweets', null, (data) => {
+
+      renderTweets(data);
+
+    });
+
+  };
+
+  loadTweets();
+
+});
