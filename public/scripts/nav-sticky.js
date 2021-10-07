8a752b9a149e010538ee938a076ea1039311500b
jvblum <jvmell.skch@gmail.com>
2021-10-07 18:50:51 -0400

    add basic responsive design stuff relative to viewport; modify some css sizes

--- public/scripts/nav-sticky.js
+++ public/scripts/nav-sticky.js
@@ -1,18 +1,21 @@
 $(document).ready(() => {
   // fix nav elements in position;
 
-  // get nav
+  // get nav & toTop
   const nav = $('nav');
+  const toTop = $('.toTop');
 
   // get nav offset position
   const sticky = nav.offset().top;
 
   const fixNav = () => {
 
-    // toggle .sticky
+    // toggle .sticky & .toTop
     if (window.pageYOffset > sticky) {
+      toTop.show();
       nav.addClass('sticky');
     } else {
+      toTop.hide();
       nav.removeClass('sticky');
     }
   };
